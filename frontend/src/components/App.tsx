a4992c136e6cdf991b9c366673294fd02c71d37c
Hairum Qureshi <hqureshi@udel.edu>
2024-07-23 14:55:59 -0400

    created buttons and landing page

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,39 @@
 import "../css/index.css";
 
+// Long Notes:
+// Titles cannot have profanity, but body text can have filtered profanity
+
+// Sticky Notes:
+// No profanity whatsoever
+
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<div className="w-full h-screen p-5">
+			<div>
+				<div className="flex items-center">
+					<h1 className="text-3xl font-semibold">
+						Anonymous Notes - Speak Your Mind
+					</h1>
+					<div className="ml-auto">
+						<button className="bg-black text-white p-2 rounded text-base mr-2">
+							Disclaimers
+						</button>
+						<button className="bg-sky-900 text-white p-2 rounded text-base 2">
+							Change Theme
+						</button>
+					</div>
+				</div>
+				<div>
+					<button className="bg-sky-700 text-white p-2 rounded text-base mt-2">
+						Create a Sticky Note
+					</button>
+				</div>
+				<div>
+					<button className="bg-slate-700 text-white p-2 rounded text-base mt-2">
+						Create a Big Note
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
