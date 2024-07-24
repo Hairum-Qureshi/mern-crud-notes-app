0ec66bb695264f1819245bab23ea6bc6294fa5cd
Hairum Qureshi <hqureshi@udel.edu>
2024-07-23 23:00:12 -0400

    implemented logic for creating a cookie when a user posts a note and implemented logic for saving note data to MongoDB

--- backend/controllers/notes_controller.ts
+++ backend/controllers/notes_controller.ts
@@ -1,10 +1,71 @@
 import { Request, Response } from "express";
+import { randomUUID } from "crypto";
+import jwt from "jsonwebtoken";
+import colors from "colors";
+import Note from "../models/Note";
+
+colors.enable();
+
+function createCookie(res: Response): string {
+	const user_id: string = randomUUID().replace(/-/g, "");
+	const payload = {
+		user_id
+	};
+	const secretKey: string = Math.floor(
+		Math.random() * Number(new Date())
+	).toString();
+	const token = jwt.sign(payload, secretKey, { expiresIn: "7d" });
+	res.cookie("anon-session", token, { httpOnly: true, maxAge: 604800000 }); // 1 week in milliseconds
+	return user_id;
+}
+
+function parseJWT(token: string): string {
+	const decoded_uid = JSON.parse(
+		Buffer.from(token.split(".")[1], "base64").toString()
+	);
+	return decoded_uid.user_id;
+}
 
 const createNote = async (req: Request, res: Response) => {
 	const { note_title, note_content } = req.body;
-	console.log(note_title, note_content);
+	const curr_uid: string | undefined = req.cookies["anon-session"];
+	if (!curr_uid) {
+		const user_id: string = createCookie(res);
+		try {
+			// Implement logic here to save note data to MongoDB
+			const createdNote = await Note.create({
+				note_title,
+				note_content,
+				curr_uid: user_id
+			});
 
-	// Check if a cookie exists; if it doesn't, create one
+			res.status(201).send(createdNote);
+		} catch (error) {
+			console.log(
+				"<notes_controller.ts> createNote function error",
+				(error as Error).toString().red.bold
+			);
+			res.status(500).send(error);
+		}
+	} else {
+		try {
+			const user_id: string = parseJWT(req.cookies["anon-session"]);
+			if (user_id) {
+				const createdNote = await Note.create({
+					note_title,
+					note_content,
+					curr_uid: user_id
+				});
+				res.status(201).send(createdNote);
+			}
+		} catch (error) {
+			console.log(
+				"<notes_controller.ts> error",
+				(error as Error).toString().red.bold
+			);
+			res.status(500).send(error);
+		}
+	}
 };
 
 export { createNote };
