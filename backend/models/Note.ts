b40dd63f838c10e4624474593568530257483aed
Hairum Qureshi <hqureshi@udel.edu>
2024-07-23 21:52:33 -0400

    created models folder and a Note model within it

---
+++ backend/models/Note.ts
@@ -0,0 +1,25 @@
+import mongoose, { InferSchemaType, Schema, model } from "mongoose";
+
+const noteSchema = new Schema(
+	{
+		note_title: {
+			type: String,
+			required: true
+		},
+		note_content: {
+			type: String,
+			required: true
+		},
+		user_id: {
+			type: String,
+			required: true,
+			unique: true
+		}
+	},
+	{
+		timestamps: true
+	}
+);
+
+type Note = InferSchemaType<typeof noteSchema>;
+export default model<Note>("Note", noteSchema);
