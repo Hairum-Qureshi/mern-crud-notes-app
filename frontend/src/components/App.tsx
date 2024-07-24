a70010aab915447be0f48a148ac1160255f4904f
Hairum Qureshi <hqureshi@udel.edu>
2024-07-24 18:39:28 -0400

    imported custom session context and wrapped all routes inside of SessionProvider context

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import NotFound from "./NotFound";
+import Form from "./Form";
+import Note from "./Note";
+import { SessionProvider } from "../contexts/sessionContext";
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+			<SessionProvider>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/new-note" element={<Form />} />
+					<Route path="/note/:note_id" element={<Note />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</SessionProvider>
 		</BrowserRouter>
 	);
 }
