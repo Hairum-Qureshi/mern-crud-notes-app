5b52d2db2d1387f95896dc74771d6cce1c64e629
Hairum Qureshi <hqureshi@udel.edu>
2024-07-23 20:15:38 -0400

    added sun/moon icons to the theme button

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
 import "../css/index.css";
+import { useState } from "react";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Long Notes:
 // Titles cannot have profanity, but body text can have filtered profanity
@@ -9,6 +13,13 @@ import "../css/index.css";
 // When a user presses the "create sticky note"/"create long note" button, create the cookie in the backend storing the uuid
 
 export default function Home() {
+	const [theme, setTheme] = useState("light");
+
+	function changeTheme() {
+		if (theme === "light") setTheme("dark");
+		else setTheme("light");
+	}
+
 	return (
 		<div className="w-full h-screen p-5">
 			<div>
@@ -20,8 +31,15 @@ export default function Home() {
 						<button className="bg-black text-white p-2 rounded text-base mr-2">
 							Disclaimers
 						</button>
-						<button className="bg-sky-900 text-white p-2 rounded text-base 2">
-							Change Theme
+						<button
+							className="bg-sky-900 text-white p-2 rounded text-base w-10"
+							onClick={changeTheme}
+						>
+							{theme == "light" ? (
+								<FontAwesomeIcon icon={faMoon} />
+							) : (
+								<FontAwesomeIcon icon={faSun} />
+							)}
 						</button>
 					</div>
 				</div>
@@ -31,9 +49,11 @@ export default function Home() {
 					</button>
 				</div>
 				<div>
-					<button className="bg-slate-700 text-white p-2 rounded text-base mt-2">
-						Create a Big Note
-					</button>
+					<Link to="/new-note">
+						<button className="bg-slate-700 text-white p-2 rounded text-base mt-2">
+							Create a Big Note
+						</button>
+					</Link>
 				</div>
 			</div>
 		</div>
