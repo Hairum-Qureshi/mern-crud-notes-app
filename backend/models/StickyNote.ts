3056dcecfdfa90328ebb103beba75b5b2bd5ffe2
Hairum Qureshi <hqureshi@udel.edu>
2024-07-25 10:27:36 -0400

    created a model for the Sticky Notes

---
+++ backend/models/StickyNote.ts
@@ -0,0 +1,28 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const stickyNoteSchema = new Schema(
+	{
+		note_title: {
+			type: String,
+			required: true
+		},
+		note_content: {
+			type: String,
+			required: true
+		},
+		curr_uid: {
+			type: String,
+			required: true
+		},
+		color: {
+			type: String,
+			default: "yellow"
+		}
+	},
+	{
+		timestamps: true
+	}
+);
+
+type StickyNote = InferSchemaType<typeof stickyNoteSchema>;
+export default model<StickyNote>("StickyNote", stickyNoteSchema);
