c60c2d04a70eaa2fead4ea925eac4e394a1441e3
Hairum Qureshi <hqureshi@udel.edu>
2024-07-25 10:07:49 -0400

    create a delete note by ID route

--- backend/routes/notes_route.ts
+++ backend/routes/notes_route.ts
@@ -1,7 +1,15 @@
 import express from "express";
-import { createNote } from "../controllers/notes_controller";
+import {
+	createNote,
+	getAllNotes,
+	getNoteData,
+	deleteNote
+} from "../controllers/notes_controller";
 const router = express.Router();
 
 router.post("/create", createNote);
+router.get("/all", getAllNotes);
+router.get("/:note_id", getNoteData);
+router.delete("/:note_id", deleteNote);
 
 export default router;
