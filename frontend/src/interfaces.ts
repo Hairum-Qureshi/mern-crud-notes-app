1a44c4e3f3cd7bc9bcf4e52c617fe11930d9fe2f
Hairum Qureshi <hqureshi@udel.edu>
2024-07-26 16:52:50 -0400

    added updatedAt property to Note interface

--- frontend/src/interfaces.ts
+++ frontend/src/interfaces.ts
@@ -5,4 +5,22 @@ export interface Note {
 	curr_uid: string;
 	containsProfanity: boolean;
 	createdAt: string;
+	updatedAt: string;
+}
+
+export interface SessionProps {
+	children: React.ReactNode;
+}
+
+export interface ContextData {
+	currUID: string | null;
+	error: string | null;
+}
+
+export interface StickyNote {
+	_id?: string;
+	note_title: string;
+	note_content: string;
+	color: string;
+	curr_uid?: string;
 }
