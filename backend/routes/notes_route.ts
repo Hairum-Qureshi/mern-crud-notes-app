d02548612f4e0759617ddd64ebe107a4567ce064
Hairum Qureshi <hqureshi@udel.edu>
2024-07-26 15:35:40 -0400

    imported newly created middleware functions and utilized them in the all the routes handling notes

--- backend/routes/notes_route.ts
+++ backend/routes/notes_route.ts
@@ -6,12 +6,13 @@ import {
 	deleteNote,
 	editNote
 } from "../controllers/notes_controller";
+import { authenticated, verifyRequest } from "../middleware/session-auth";
 const router = express.Router();
 
 router.post("/create", createNote);
 router.get("/all", getAllNotes);
 router.get("/:note_id", getNoteData);
-router.delete("/:note_id", deleteNote);
-router.patch("/:note_id/edit", editNote);
+router.delete("/:note_id", authenticated, verifyRequest, deleteNote);
+router.patch("/:note_id/edit", authenticated, verifyRequest, editNote);
 
 export default router;
