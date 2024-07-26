05638298c78f05f512e1be6fe03c67f9c2336405
Hairum Qureshi <hqureshi@udel.edu>
2024-07-26 18:30:31 -0400

    implemented loading animation if all notes haven't been displayed yet

--- frontend/src/components/NotesViewer.tsx
+++ frontend/src/components/NotesViewer.tsx
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 import useNotes from "../hooks/useNotes";
 import { Note } from "../interfaces";
 import useSessionContext from "../contexts/sessionContext";
+import { tailspin } from "ldrs";
 
-// TODO - maybe add a loading animation here too
+// TODO - consider adding pagination
 
 export default function NotesViewer() {
-	const { allNotesData } = useNotes();
+	const { allNotesData, loadingStatus } = useNotes();
 	const { currUID } = useSessionContext()!;
 
-	console.log("x", currUID);
+	tailspin.register();
 
 	function formatDate(utcDate: string): string {
 		const date = new Date(utcDate);
@@ -42,62 +43,76 @@ export default function NotesViewer() {
 				<h1 className="text-3xl font-semibold mb-1">View All Posted Notes</h1>
 				<p className="text-base">Click on the blog title to go to that blog</p>
 			</div>
-			<table className="w-full table-auto">
-				<thead className="bg-slate-700 text-white border-b-2 border-gray-200">
-					<tr>
-						<th className="p-3 text-base font-semibold tracking-wide text-left">
-							Blog Title
-						</th>
-						<th className="p-3 text-base font-semibold tracking-wide text-left">
-							Owner
-						</th>
-						<th className="p-3 text-base font-semibold tracking-wide text-left">
-							Flag
-						</th>
-						<th className="p-3 text-base font-semibold tracking-wide text-left">
-							Posted Date
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{allNotesData.map((note: Note, index: number) => {
-						return (
-							<>
-								<tr
-									className={`${index % 2 === 0 ? "bg-white" : "bg-slate-200"}`}
-								>
-									<Link to={`/note/${note._id}`}>
+			{!loadingStatus ? (
+				<table className="w-full table-auto">
+					<thead className="bg-slate-700 text-white border-b-2 border-gray-200">
+						<tr>
+							<th className="p-3 text-base font-semibold tracking-wide text-left">
+								Blog Title
+							</th>
+							<th className="p-3 text-base font-semibold tracking-wide text-left">
+								Owner
+							</th>
+							<th className="p-3 text-base font-semibold tracking-wide text-left">
+								Flag
+							</th>
+							<th className="p-3 text-base font-semibold tracking-wide text-left">
+								Posted Date
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{allNotesData.map((note: Note, index: number) => {
+							return (
+								<>
+									<tr
+										className={`${
+											index % 2 === 0 ? "bg-white" : "bg-slate-200"
+										}`}
+									>
+										<Link to={`/note/${note._id}`}>
+											<td className="p-3 text-base text-gray-700">
+												{note.note_title}
+											</td>
+										</Link>
 										<td className="p-3 text-base text-gray-700">
-											{note.note_title}
+											{note.curr_uid === currUID ? (
+												<>{<FontAwesomeIcon icon={faCrown} />} You</>
+											) : (
+												"Anonymous"
+											)}
 										</td>
-									</Link>
-									<td className="p-3 text-base text-gray-700">
-										{note.curr_uid === currUID ? (
-											<>{<FontAwesomeIcon icon={faCrown} />} You</>
-										) : (
-											"Anonymous"
-										)}
-									</td>
-									<td className="p-3 text-base text-gray-700 flex justify-left">
-										{note.containsProfanity ? (
-											<div className="inline-block bg-red-600 text-white px-2 py-1 items-center rounded text-base">
-												Explicit
-											</div>
-										) : (
-											<div className="inline-flex bg-green-600 text-white px-2 py-1 rounded text-base items-center">
-												Not Explicit
-											</div>
-										)}
-									</td>
-									<td className="p-3 text-base text-gray-700">
-										{formatDate(note.createdAt)}
-									</td>
-								</tr>
-							</>
-						);
-					})}
-				</tbody>
-			</table>
+										<td className="p-3 text-base text-gray-700 flex justify-left">
+											{note.containsProfanity ? (
+												<div className="inline-block bg-red-600 text-white px-2 py-1 items-center rounded text-base">
+													Explicit
+												</div>
+											) : (
+												<div className="inline-flex bg-green-600 text-white px-2 py-1 rounded text-base items-center">
+													Not Explicit
+												</div>
+											)}
+										</td>
+										<td className="p-3 text-base text-gray-700">
+											{formatDate(note.createdAt)}
+										</td>
+									</tr>
+								</>
+							);
+						})}
+					</tbody>
+				</table>
+			) : (
+				<div className="flex items-center justify-center mt-20">
+					<l-tailspin
+						size="40"
+						stroke="5"
+						speed="0.9"
+						color="black"
+					></l-tailspin>
+					<h1 className="text-2xl font-semibold ml-3">LOADING NOTES</h1>
+				</div>
+			)}
 		</div>
 	);
 }
