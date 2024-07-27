c0e9a5ed35419d8dbd379696580efc67452e24e5
Hairum Qureshi <hqureshi@udel.edu>
2024-07-27 13:53:05 -0400

    installed express-rate-limit and added limiter middleware to POST and PATCH request

--- backend/routes/notes_route.ts
+++ backend/routes/notes_route.ts
@@ -8,11 +8,18 @@ import {
 } from "../controllers/notes_controller";
 import { authenticated, verifyRequest } from "../middleware/session-auth";
 const router = express.Router();
+import rateLimit from "express-rate-limit";
 
-router.post("/create", createNote);
+const limit = rateLimit({
+	max: 10, // maximum requests
+	windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
+	message: "Too many requests. Please try again in 1 hour."
+});
+
+router.post("/create", limit, createNote);
 router.get("/all", getAllNotes);
 router.get("/:note_id", getNoteData);
 router.delete("/:note_id", authenticated, verifyRequest, deleteNote);
-router.patch("/:note_id/edit", authenticated, verifyRequest, editNote);
+router.patch("/:note_id/edit", authenticated, verifyRequest, limit, editNote);
 
 export default router;
