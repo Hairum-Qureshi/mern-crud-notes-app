56afb8b78688cf5ed1ee4f5fd3baf374efcca07e
Hairum Qureshi <hqureshi@udel.edu>
2024-07-27 22:10:05 -0400

    made navbar mobile-friendly and implemented hamburger-menu logic

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,11 @@
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faMoon, faSun, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import useStickyNotes from "../hooks/useStickyNotes";
 import { useState } from "react";
 
 export default function Navbar() {
+	const [openNavbar, setOpenNavbar] = useState(false);
 	const { createStickyNote } = useStickyNotes();
 	const [theme, setTheme] = useState("light");
 
@@ -14,53 +15,122 @@ export default function Navbar() {
 	}
 
 	return (
-		<div className="w-full p-2 flex items-center mx-2">
-			<Link to="/">
-				<h1 className="text-2xl">Anonymous Notes - Speak Your Mind!</h1>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/new-note">
-					<button className="text-white p-2 rounded text-base mr-2">
-						Create a Big Note
-					</button>
-				</Link>
-				<Link to="/notes/all">
-					<button className="text-white p-2 rounded text-base mr-2">
-						View Notes
-					</button>
-				</Link>
-				<Link to="/sticky-notes">
-					<button
-						className="text-white p-2 rounded text-base mr-2"
-						onClick={createStickyNote}
+		<div className="w-full p-2 items-center relative">
+			<div className="flex">
+				<Link to="/">
+					<h1
+						className="lg:text-2xl text-xl"
+						onClick={() => setOpenNavbar(false)}
 					>
-						Sticky Notes
-					</button>
+						Anonymous Notes - Speak Your Mind!
+					</h1>
 				</Link>
-				<Link to="/important">
-					<button className="text-white p-2 rounded text-base mr-2">
-						Disclaimers & Rules
-					</button>
-				</Link>
-				<Link to="/contact">
+				<div className="lg:flex items-center ml-auto hidden">
+					<Link to="/new-note" onClick={() => setOpenNavbar(false)}>
+						<button className="text-white p-2 text-center block w-full">
+							Create a Big Note
+						</button>
+					</Link>
+					<Link to="/notes/all" onClick={() => setOpenNavbar(false)}>
+						<button className="text-white p-2 text-center block w-full">
+							View Notes
+						</button>
+					</Link>
+					<Link to="/sticky-notes" onClick={() => setOpenNavbar(false)}>
+						<button
+							className="text-white p-2 text-center block w-full"
+							onClick={createStickyNote}
+						>
+							Sticky Notes
+						</button>
+					</Link>
+					<Link to="/important" onClick={() => setOpenNavbar(false)}>
+						<button className="text-white p-2 text-center block w-full">
+							Disclaimers & Rules
+						</button>
+					</Link>
+					<Link to="/contact" onClick={() => setOpenNavbar(false)}>
+						<button
+							className="text-white p-2 text-center block w-full"
+							onClick={createStickyNote}
+						>
+							Contact
+						</button>
+					</Link>
 					<button
-						className="text-white p-2 rounded text-base mr-2"
-						onClick={createStickyNote}
+						className="bg-sky-800 text-white p-2 rounded text-base w-10"
+						onClick={changeTheme}
 					>
-						Contact
+						{theme == "light" ? (
+							<FontAwesomeIcon icon={faMoon} />
+						) : (
+							<FontAwesomeIcon icon={faSun} />
+						)}
 					</button>
-				</Link>
-				<button
-					className="bg-sky-800 text-white p-2 rounded text-base w-10"
-					onClick={changeTheme}
-				>
-					{theme == "light" ? (
-						<FontAwesomeIcon icon={faMoon} />
+				</div>
+				<div className="ml-auto flex items-center lg:hidden">
+					{!openNavbar ? (
+						<FontAwesomeIcon
+							icon={faBars}
+							className="text-2xl"
+							onClick={() => setOpenNavbar(true)}
+						/>
 					) : (
-						<FontAwesomeIcon icon={faSun} />
+						<FontAwesomeIcon
+							icon={faX}
+							className="text-2xl"
+							onClick={() => setOpenNavbar(false)}
+						/>
 					)}
-				</button>
+				</div>
 			</div>
+			{openNavbar && (
+				<div className="absolute lg:hidden left-0 z-10 bg-slate-900 w-full h-screen flex text-center items-center justify-center">
+					<div className="text-3xl mt-5">
+						<Link to="/new-note" onClick={() => setOpenNavbar(false)}>
+							<button className="text-white p-2 text-center block w-full">
+								Create a Big Note
+							</button>
+						</Link>
+						<Link to="/notes/all" onClick={() => setOpenNavbar(false)}>
+							<button className="text-white p-2 text-center block w-full">
+								View Notes
+							</button>
+						</Link>
+						<Link to="/sticky-notes" onClick={() => setOpenNavbar(false)}>
+							<button
+								className="text-white p-2 text-center block w-full"
+								onClick={createStickyNote}
+							>
+								Sticky Notes
+							</button>
+						</Link>
+						<Link to="/important" onClick={() => setOpenNavbar(false)}>
+							<button className="text-white p-2 text-center block w-full">
+								Disclaimers & Rules
+							</button>
+						</Link>
+						<Link to="/contact" onClick={() => setOpenNavbar(false)}>
+							<button
+								className="text-white p-2 text-center block w-full"
+								onClick={createStickyNote}
+							>
+								Contact
+							</button>
+						</Link>
+						<button
+							className="bg-sky-800 text-white p-2 rounded mt-5 w-14"
+							onClick={changeTheme}
+						>
+							{theme == "light" ? (
+								<FontAwesomeIcon icon={faMoon} />
+							) : (
+								<FontAwesomeIcon icon={faSun} />
+							)}
+						</button>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
