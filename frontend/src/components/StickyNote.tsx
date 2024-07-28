3cb8f2994ba51b2ae08b89daad9b071e406b1e56
Hairum Qureshi <hqureshi@udel.edu>
2024-07-28 00:36:12 -0400

    removed white background color CSS from textarea

--- frontend/src/components/StickyNote.tsx
+++ frontend/src/components/StickyNote.tsx
@@ -1,16 +1,23 @@
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { StickyNote as StickyNoteInterface } from "../interfaces";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useStickyNotes from "../hooks/useStickyNotes";
 
 interface Props {
 	stickyNote: StickyNoteInterface;
 }
 
 export default function StickyNote({ stickyNote }: Props) {
-	const [stickyNotetitle, setStickyNoteTitle] = useState("");
+	const [stickyNoteTitle, setStickyNoteTitle] = useState("");
 	const [stickyNoteBody, setStickyNoteBody] = useState("");
 	const [stickyNoteColor, setStickyNoteColor] = useState("yellow");
+
+	const { saveStickyNoteData } = useStickyNotes();
+
+	useEffect(() => {
+		saveStickyNoteData(stickyNoteTitle, stickyNoteBody, stickyNoteColor);
+	}, [stickyNoteTitle, stickyNoteBody, stickyNoteColor]);
 
 	return (
 		<div
@@ -39,7 +46,7 @@ export default function StickyNote({ stickyNote }: Props) {
 					onClick={() => setStickyNoteColor("purple")}
 				></div>
 				<div className="ml-auto px-2 py-1 rounded bg-red-600 text-white text-base hover:cursor-pointer">
-					<FontAwesomeIcon icon={faTrash} />{" "}
+					<FontAwesomeIcon icon={faTrash} />
 				</div>
 			</div>
 			<textarea
@@ -49,7 +56,7 @@ export default function StickyNote({ stickyNote }: Props) {
 			></textarea>
 			<textarea
 				placeholder="Enter note"
-				className="outline-none -mt-3 bg-white resize-none p-2 w-full h-44 max-h-44 overflow-hidden bg-transparent rounded"
+				className="outline-none -mt-3 resize-none p-2 w-full h-44 max-h-44 overflow-hidden bg-transparent rounded"
 				onChange={e => setStickyNoteBody(e.target.value)}
 			></textarea>
 		</div>
