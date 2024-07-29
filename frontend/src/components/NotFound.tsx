33a43037ba327b15eb8d71e55f5f8d6d4a403f20
Hairum Qureshi <hqureshi@udel.edu>
2024-07-29 17:57:47 -0400

    added dark mode theme styling to 404 page

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,7 +1,13 @@
+import { useTheme } from "../contexts/themeContext";
+
 export default function NotFound() {
+	const { theme } = useTheme()!;
+
 	return (
-		<div className="flex flex-row min-h-screen text-3xl font-semibold justify-center items-center">
-			404 - Page Not Found!
+		<div className={`${theme === "dark" ? "dark" : ""}`}>
+			<div className="flex flex-row lg:h-[calc(100vh-3.5rem)] h-[calc(100vh-2.5rem)] dark:bg-slate-800 dark:text-slate-50 text-3xl font-semibold justify-center items-center">
+				404 - Page Not Found!
+			</div>
 		</div>
 	);
 }
