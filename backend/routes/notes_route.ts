8dabb818829a1946960339b4469c78e0c3ccad99
Hairum Qureshi <hqureshi@udel.edu>
2024-07-28 20:28:01 -0400

    removed express rate limit config from notes route

--- backend/routes/notes_route.ts
+++ backend/routes/notes_route.ts
@@ -8,13 +8,7 @@ import {
 } from "../controllers/notes_controller";
 import { authenticated, verifyRequest } from "../middleware/session-auth";
 const router = express.Router();
-import rateLimit from "express-rate-limit";
-
-const limit = rateLimit({
-	max: 10, // maximum requests
-	windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
-	message: "Too many requests. Please try again in 1 hour."
-});
+import limit from "../config/rate-limiter";
 
 router.post("/create", limit, createNote);
 router.get("/all", getAllNotes);
