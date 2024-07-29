5e393181bce556a687c48ec219df1a68e0ff1ea2
Hairum Qureshi <hqureshi@udel.edu>
2024-07-28 23:47:38 -0400

    implemented logic for displaying all posted sticky notes

--- frontend/src/components/StickyNotesDisplay.tsx
+++ frontend/src/components/StickyNotesDisplay.tsx
@@ -1,3 +1,33 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useStickyNotes from "../hooks/useStickyNotes";
+import { StickyNote } from "../interfaces";
+import StickyNoteComponent from "./StickyNote";
+
 export default function StickyNotesDisplay() {
-	return <div>StickyNotesDisplay</div>;
+	const { createStickyNote, stickyNotes } = useStickyNotes();
+
+	return (
+		<div className="w-full p-3">
+			<div className="p-5 text-2xl">
+				<button
+					className="bg-slate-200 p-2 rounded-md border border-black w-12 h-12 flex items-center justify-center"
+					onClick={createStickyNote}
+				>
+					<FontAwesomeIcon icon={faPlus} />
+				</button>
+			</div>
+			<div className="w-full p-5 h-auto flex flex-wrap items-center justify-center">
+				{stickyNotes.length > 0 &&
+					stickyNotes.map((stickyNote: StickyNote, index: number) => {
+						return (
+							<StickyNoteComponent
+								stickyNote={stickyNote}
+								key={stickyNote._id || index}
+							/>
+						);
+					})}
+			</div>
+		</div>
+	);
 }
