c532858708ce5643cd8a3fb0be5e568f98145e53
Hairum Qureshi <hqureshi@udel.edu>
2024-07-28 23:45:01 -0400

    updated verifyRequest function middleware logic so it can work for the sticky notes routes as well

--- backend/middleware/session-auth.ts
+++ backend/middleware/session-auth.ts
@@ -4,6 +4,8 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import colors from "colors";
 import Note from "../models/Note";
 import { createCookie } from "../controllers/notes_controller";
+import StickyNote from "../models/StickyNote";
+import mongoose from "mongoose";
 
 colors.enable();
 dotenv.config();
@@ -19,10 +21,10 @@ const authenticated = (req: Request, res: Response, next: NextFunction) => {
 			secret,
 			(err: Error | null, decoded: string | JwtPayload | undefined) => {
 				if (err) {
-					// console.log(
-					// 	"<session-auth.ts> middleware".yellow.bold,
-					// 	(err as Error).toString().red.bold
-					// );
+					console.log(
+						"<session-auth.ts> middleware".yellow.bold,
+						(err as Error).toString().red.bold
+					);
 					return res.status(401).json({ message: "Invalid token" });
 				} else {
 					const decoded_parsed: JwtPayload = decoded as JwtPayload;
@@ -48,16 +50,38 @@ const verifyRequest = async (
 ) => {
 	try {
 		const decoded_uid: string = req.cookies.decoded_uid;
-		if (req.params.note_id) {
-			const note = await Note.findById(req.params.note_id);
-			if (!note) {
-				return res.status(404).json({ message: "Note not found" });
-			}
+		if (req.params) {
+			const { note_id, sticky_note_id } = req.params;
+			if (note_id) {
+				if (mongoose.isValidObjectId(note_id)) {
+					const note = await Note.findById(note_id);
+					if (!note) {
+						return res.status(404).json({ message: "Note not found" });
+					}
 
-			if (note.curr_uid !== decoded_uid) {
-				return res
-					.status(403)
-					.json({ message: "You are not the owner of this note" });
+					if (note?.curr_uid !== decoded_uid) {
+						return res
+							.status(403)
+							.json({ message: "You are not the owner of this note" });
+					}
+				} else {
+					return res.status(400).json({ message: "Invalid note ID" });
+				}
+			} else {
+				if (mongoose.isValidObjectId(sticky_note_id)) {
+					const sticky_note = await StickyNote.findById(sticky_note_id);
+					if (!sticky_note) {
+						return res.status(404).json({ message: "Sticky note not found" });
+					} else {
+						if (sticky_note?.curr_uid !== decoded_uid) {
+							return res
+								.status(403)
+								.json({ message: "You are not the owner of this sticky note" });
+						}
+					}
+				} else {
+					return res.status(400).json({ message: "Invalid sticky note ID" });
+				}
 			}
 
 			next();
@@ -65,8 +89,9 @@ const verifyRequest = async (
 			res.status(400).json({ message: "Note ID is required" });
 		}
 	} catch (error) {
+		console.log("x");
 		console.log(
-			"<session-auth.ts> middleware verifyRequest function error",
+			"<session-auth.ts> middleware verifyRequest function error".yellow,
 			(error as Error).toString().red.bold
 		);
 		res.status(500).send({ message: "Server error" });
