0cbf9ebf41657e7a08932f6974277cc043fb763b
Hairum Qureshi <hqureshi@udel.edu>
2024-07-29 07:53:48 -0400

    imported newly created themeContext in and wrapped it around child components

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -9,26 +9,29 @@ import Disclaimer from "../components/Disclaimer";
 import Contact from "./Contact";
 import Navbar from "./Navbar";
 import StickyNotesDisplay from "./StickyNotesDisplay";
+import { ThemeProvider } from "../contexts/themeContext";
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<SessionProvider>
-				<div className="flex items-center bg-slate-900 text-white">
-					<Navbar />
-				</div>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/important" element={<Disclaimer />} />
-					<Route path="/sticky-notes" element={<StickyNotesDisplay />} />
-					<Route path="/new-note" element={<Form />} />
-					<Route path="/note/:note_id" element={<Note />} />
-					<Route path="/note/:note_id/edit" element={<Form />} />
-					<Route path="/notes/all" element={<NotesViewer />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</SessionProvider>
+			<ThemeProvider>
+				<SessionProvider>
+					<div className="flex items-center bg-slate-900 text-white">
+						<Navbar />
+					</div>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/important" element={<Disclaimer />} />
+						<Route path="/sticky-notes" element={<StickyNotesDisplay />} />
+						<Route path="/new-note" element={<Form />} />
+						<Route path="/note/:note_id" element={<Note />} />
+						<Route path="/note/:note_id/edit" element={<Form />} />
+						<Route path="/notes/all" element={<NotesViewer />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</SessionProvider>
+			</ThemeProvider>
 		</BrowserRouter>
 	);
 }
