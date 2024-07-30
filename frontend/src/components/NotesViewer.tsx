9c07a9e567623233eb8f926c3bf676b9f0817670
Hairum Qureshi <hqureshi@udel.edu>
2024-07-29 22:27:45 -0400

    updated styling and implemented dark mode theme styles

--- frontend/src/components/NotesViewer.tsx
+++ frontend/src/components/NotesViewer.tsx
@@ -10,12 +10,7 @@ import { Note } from "../interfaces";
 import useSessionContext from "../contexts/sessionContext";
 import { tailspin } from "ldrs";
 import { useEffect, useState } from "react";
-
-// TODO - consider scenario where there's tons of buttons for each page; implement some type of logic to only list X number of buttons and if there's number of pages beyond that, maybe create a button that has ellipses?
-// TODO - implement logic to render out any error messages to the user
-// TODO - if there are no notes, add a message saying there are no notes
-// TODO - consider making the pagination logic a separate component
-// ! MINOR BUG - if you're on the first page and click the right paginate button, you have to click it twice to get to the second page
+import { useTheme } from "../contexts/themeContext";
 
 export default function NotesViewer() {
 	const [numButtons, setNumButtons] = useState<number[]>([]);
@@ -23,6 +18,7 @@ export default function NotesViewer() {
 	const { currUID } = useSessionContext()!;
 	const [searchParams, setSearchParams] = useSearchParams();
 	const pageNumber = searchParams.get("page");
+	const { theme } = useTheme()!;
 
 	useEffect(() => {
 		const updateButtons = () => {
@@ -39,8 +35,6 @@ export default function NotesViewer() {
 
 		updateButtons();
 	}, [numPages]);
-
-	console.log(numPages);
 
 	tailspin.register();
 
@@ -60,16 +54,10 @@ export default function NotesViewer() {
 	}
 
 	return (
-		<>
-			<div className="lg:p-10 p-3 h-screen w-full lg:-mt-5">
-				{/* <Link to="/">
-					<div className="flex items-center text-lg">
-						<FontAwesomeIcon icon={faArrowLeftLong} />
-						<h1 className="ml-2">Go Back</h1>
-					</div>
-				</Link> */}
+		<div className={`${theme === "dark" ? "dark" : ""}`}>
+			<div className="lg:p-10 p-3 lg:h-[calc(100vh-3.5rem)] w-full dark:bg-slate-800 dark:text-slate-50">
 				<div className="mb-3 relative">
-					<h1 className="text-3xl font-semibold mb-1">
+					<h1 className="text-3xl font-semibold mb-1 lg:-mt-4">
 						View All Posted Notes ({totalNotes})
 					</h1>
 					<p className="text-base">
@@ -80,7 +68,7 @@ export default function NotesViewer() {
 					<>
 						<div className="overflow-x-auto pb-20">
 							<table className="w-full table-auto">
-								<thead className="bg-slate-700 text-white border-b-2 border-gray-200">
+								<thead className="bg-slate-700 dark:bg-slate-900 text-white dark:text-white">
 									<tr>
 										<th className="p-3 text-base font-semibold tracking-wide text-left">
 											Blog Title
@@ -100,17 +88,19 @@ export default function NotesViewer() {
 									{allNotesData.map((note: Note, index: number) => {
 										return (
 											<tr
-												key={note._id} // Added key prop
+												key={note._id}
 												className={`${
-													index % 2 === 0 ? "bg-white" : "bg-slate-200"
+													index % 2 === 0
+														? "bg-white dark:bg-slate-500"
+														: "bg-slate-200 dark:bg-slate-700"
 												}`}
 											>
 												<Link to={`/note/${note._id}`}>
-													<td className="p-3 text-base text-gray-700">
+													<td className="p-3 text-base text-gray-700 dark:text-slate-50">
 														{note.note_title}
 													</td>
 												</Link>
-												<td className="p-3 text-base text-gray-700">
+												<td className="p-3 text-base text-gray-700 dark:text-slate-50">
 													{note.curr_uid === currUID ? (
 														<>{<FontAwesomeIcon icon={faCrown} />} You</>
 													) : (
@@ -119,16 +109,16 @@ export default function NotesViewer() {
 												</td>
 												<td className="p-3 text-base text-gray-700 flex justify-left">
 													{note.containsProfanity ? (
-														<div className="inline-block bg-red-600 text-white px-2 py-1 items-center rounded text-base">
+														<div className="inline-block bg-red-600 dark:bg-red-700 text-white px-2 py-1 items-center rounded text-base">
 															Explicit
 														</div>
 													) : (
-														<div className="inline-flex bg-green-600 text-white px-2 py-1 rounded text-base items-center">
+														<div className="inline-flex bg-green-600 dark:bg-green-700 text-white px-2 py-1 rounded text-base items-center">
 															Not Explicit
 														</div>
 													)}
 												</td>
-												<td className="p-3 text-base text-gray-700">
+												<td className="p-3 text-base text-gray-700 dark:text-slate-50">
 													{formatDate(note.createdAt)}
 												</td>
 											</tr>
@@ -139,28 +129,31 @@ export default function NotesViewer() {
 						</div>
 					</>
 				) : (
-					<div className="flex items-center justify-center mt-20">
+					<div className="flex items-center justify-center mt-20 dark:text-slate-50">
 						<l-tailspin
 							size="40"
 							stroke="5"
 							speed="0.9"
-							color="black"
+							color={theme === "dark" ? "white" : "black"}
 						></l-tailspin>
+
 						<h1 className="text-2xl font-semibold ml-3">LOADING NOTES</h1>
 					</div>
 				)}
 			</div>
 			{totalNotes > 9 && (
-				<div className="w-full lg:absolute fixed bottom-0 p-5 flex items-center text-lg justify-center bg-white shadow-md">
+				<div className="w-full lg:absolute fixed bottom-0 p-5 flex items-center text-lg justify-center shadow-md">
 					<Link
 						to={
 							Number(pageNumber) <= 1
 								? "/notes/all"
 								: `?page=${Number(pageNumber) - 1}`
 						}
-						className={`w-9 h-9 rounded p-2 bg-black text-white flex items-center justify-center mr-2 ${
-							Number(pageNumber) <= 1 ? "pointer-events-none bg-red-900" : ""
-						}`}
+						className={`w-9 h-9 rounded p-2 ${
+							Number(pageNumber) <= 1
+								? "pointer-events-none bg-red-800 dark:bg-red-800 text-white"
+								: "bg-black dark:bg-slate-900 text-white dark:text-slate-50"
+						} flex items-center justify-center mr-2`}
 					>
 						<FontAwesomeIcon icon={faArrowLeft} />
 					</Link>
@@ -170,8 +163,10 @@ export default function NotesViewer() {
 								key={buttonNo}
 								to={`?page=${buttonNo}`}
 								className={`w-9 h-9 rounded p-2 ${
-									Number(pageNumber) === buttonNo ? "bg-slate-400" : "bg-black"
-								} text-white flex items-center justify-center mr-2`}
+									Number(pageNumber) === buttonNo
+										? "bg-slate-400 dark:bg-slate-600 text-white"
+										: "bg-black dark:bg-slate-900 text-white dark:text-slate-50"
+								} flex items-center justify-center mr-2`}
 							>
 								{buttonNo}
 							</Link>
@@ -179,211 +174,16 @@ export default function NotesViewer() {
 					})}
 					<Link
 						to={`?page=${Number(pageNumber) + 1}`}
-						className={`w-9 h-9 rounded p-2 bg-black text-white flex items-center justify-center mr-2 ${
-							Number(pageNumber) >= numPages && "pointer-events-none bg-red-900"
-						}`}
+						className={`w-9 h-9 rounded p-2 ${
+							Number(pageNumber) >= numPages
+								? "pointer-events-none bg-red-800 dark:bg-red-800 text-white"
+								: "bg-black dark:bg-slate-900 text-white dark:text-slate-50"
+						} flex items-center justify-center mr-2`}
 					>
 						<FontAwesomeIcon icon={faArrowRight} />
 					</Link>
 				</div>
 			)}
-		</>
+		</div>
 	);
 }
-
-// import {
-// 	faArrowLeft,
-// 	faArrowLeftLong,
-// 	faArrowRight,
-// 	faCrown
-// } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { Link, useSearchParams } from "react-router-dom";
-// import useNotes from "../hooks/useNotes";
-// import { Note } from "../interfaces";
-// import useSessionContext from "../contexts/sessionContext";
-// import { tailspin } from "ldrs";
-// import { useEffect, useState } from "react";
-
-// // TODO - consider scenario where there's tons of buttons for each page; implement some type of logic to only list X number of buttons and if there's number of pages beyond that, maybe create a button that has ellipses?
-// // TODO - implement logic to render out any error messages to the user
-// // TODO - if there are no notes, add a message saying there are no notes
-// // TODO - consider making the pagination logic a separate component
-
-// export default function NotesViewer() {
-// 	const [numButtons, setNumButtons] = useState<number[]>([]);
-// 	const { allNotesData, loadingStatus, numPages, totalNotes } = useNotes();
-// 	const { currUID } = useSessionContext()!;
-// 	const [searchParams, setSearchParams] = useSearchParams();
-// 	const pageNumber = searchParams.get("page");
-
-// 	useEffect(() => {
-// 		const updateButtons = () => {
-// 			if (numPages <= 0) {
-// 				setNumButtons([]);
-// 			} else {
-// 				const buttons = [];
-// 				for (let i = 1; i <= numPages; i++) {
-// 					buttons.push(i);
-// 				}
-// 				setNumButtons(buttons);
-// 			}
-// 		};
-
-// 		updateButtons();
-// 	}, [numPages]);
-
-// 	console.log(numPages);
-
-// 	tailspin.register();
-
-// 	function formatDate(utcDate: string): string {
-// 		const date = new Date(utcDate);
-
-// 		const options: Intl.DateTimeFormatOptions = {
-// 			year: "numeric",
-// 			month: "long",
-// 			day: "numeric",
-// 			hour: "2-digit",
-// 			minute: "2-digit"
-// 		};
-
-// 		const formattedDate = date.toLocaleDateString("en-US", options);
-// 		return formattedDate;
-// 	}
-
-// 	console.log(numPages);
-
-// 	return (
-// 		<>
-// 			<div className="lg:p-10 p-3 h-screen w-full">
-// 				<Link to="/">
-// 					<div className="flex items-center text-lg">
-// 						<FontAwesomeIcon icon={faArrowLeftLong} />
-// 						<h1 className="ml-2">Go Back</h1>
-// 					</div>
-// 				</Link>
-// 				<div className="mb-3 relative">
-// 					<h1 className="text-3xl font-semibold mb-1">
-// 						View All Posted Notes ({totalNotes})
-// 					</h1>
-// 					<p className="text-base">
-// 						Click on the blog title to go to that blog
-// 					</p>
-// 				</div>
-// 				{!loadingStatus ? (
-// 					<>
-// 						<div className="overflow-x-auto pb-20">
-// 							{" "}
-// 							{/* Added pb-20 for padding bottom */}
-// 							<table className="w-full table-auto">
-// 								<thead className="bg-slate-700 text-white border-b-2 border-gray-200">
-// 									<tr>
-// 										<th className="p-3 text-base font-semibold tracking-wide text-left">
-// 											Blog Title
-// 										</th>
-// 										<th className="p-3 text-base font-semibold tracking-wide text-left">
-// 											Owner
-// 										</th>
-// 										<th className="p-3 text-base font-semibold tracking-wide text-left">
-// 											Flag
-// 										</th>
-// 										<th className="p-3 text-base font-semibold tracking-wide text-left">
-// 											Posted Date
-// 										</th>
-// 									</tr>
-// 								</thead>
-// 								<tbody>
-// 									{allNotesData.map((note: Note, index: number) => {
-// 										return (
-// 											<tr
-// 												key={note._id} // Added key prop
-// 												className={`${
-// 													index % 2 === 0 ? "bg-white" : "bg-slate-200"
-// 												}`}
-// 											>
-// 												<Link to={`/note/${note._id}`}>
-// 													<td className="p-3 text-base text-gray-700">
-// 														{note.note_title}
-// 													</td>
-// 												</Link>
-// 												<td className="p-3 text-base text-gray-700">
-// 													{note.curr_uid === currUID ? (
-// 														<>{<FontAwesomeIcon icon={faCrown} />} You</>
-// 													) : (
-// 														"Anonymous"
-// 													)}
-// 												</td>
-// 												<td className="p-3 text-base text-gray-700 flex justify-left">
-// 													{note.containsProfanity ? (
-// 														<div className="inline-block bg-red-600 text-white px-2 py-1 items-center rounded text-base">
-// 															Explicit
-// 														</div>
-// 													) : (
-// 														<div className="inline-flex bg-green-600 text-white px-2 py-1 rounded text-base items-center">
-// 															Not Explicit
-// 														</div>
-// 													)}
-// 												</td>
-// 												<td className="p-3 text-base text-gray-700">
-// 													{formatDate(note.createdAt)}
-// 												</td>
-// 											</tr>
-// 										);
-// 									})}
-// 								</tbody>
-// 							</table>
-// 						</div>
-// 					</>
-// 				) : (
-// 					<div className="flex items-center justify-center mt-20">
-// 						<l-tailspin
-// 							size="40"
-// 							stroke="5"
-// 							speed="0.9"
-// 							color="black"
-// 						></l-tailspin>
-// 						<h1 className="text-2xl font-semibold ml-3">LOADING NOTES</h1>
-// 					</div>
-// 				)}
-// 			</div>
-// 			{totalNotes > 9 && (
-// 				<div className="w-full lg:absolute fixed bottom-0 p-5 flex items-center text-lg justify-center bg-white shadow-md">
-// 					<Link
-// 						to={
-// 							Number(pageNumber) <= 1
-// 								? "/notes/all"
-// 								: `?page=${Number(pageNumber) - 1}`
-// 						}
-// 						className={`w-9 h-9 rounded p-2 bg-black text-white flex items-center justify-center mr-2 ${
-// 							Number(pageNumber) <= 1 ? "pointer-events-none bg-red-900" : ""
-// 						}`}
-// 					>
-// 						<FontAwesomeIcon icon={faArrowLeft} />
-// 					</Link>
-// 					{numButtons.map((buttonNo: number) => {
-// 						return (
-// 							<Link
-// 								key={buttonNo}
-// 								to={`?page=${buttonNo}`}
-// 								className={`w-9 h-9 rounded p-2 ${
-// 									Number(pageNumber) === buttonNo ? "bg-slate-400" : "bg-black"
-// 								} text-white flex items-center justify-center mr-2`}
-// 							>
-// 								{buttonNo}
-// 							</Link>
-// 						);
-// 					})}
-// 					<Link
-// 						to={`?page=${Number(pageNumber) + 1}`}
-// 						className={`w-9 h-9 rounded p-2 bg-black text-white flex items-center justify-center mr-2 ${
-// 							Number(pageNumber) >= numPages && "pointer-events-none bg-red-900"
-// 						}`}
-// 					>
-// 						<FontAwesomeIcon icon={faArrowRight} />
-// 					</Link>
-// 				</div>
-// 			)}
-// 		</>
-// 	);
-// }
