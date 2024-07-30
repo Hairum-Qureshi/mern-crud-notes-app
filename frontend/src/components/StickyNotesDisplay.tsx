7788bf7381e2d539b1cd4f9d8c97b5ae458ad7e1
Hairum Qureshi <hqureshi@udel.edu>
2024-07-29 23:31:23 -0400

    added dark mode color theme

--- frontend/src/components/StickyNotesDisplay.tsx
+++ frontend/src/components/StickyNotesDisplay.tsx
@@ -3,30 +3,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useStickyNotes from "../hooks/useStickyNotes";
 import { StickyNote } from "../interfaces";
 import StickyNoteComponent from "./StickyNote";
+import { useTheme } from "../contexts/themeContext";
 
 export default function StickyNotesDisplay() {
 	const { createStickyNote, stickyNotes } = useStickyNotes();
+	const { theme } = useTheme()!;
 
 	return (
-		<div className="w-full p-3">
-			<div className="p-5 text-2xl">
-				<button
-					className="bg-slate-200 p-2 rounded-md border border-black w-12 h-12 flex items-center justify-center"
-					onClick={createStickyNote}
-				>
-					<FontAwesomeIcon icon={faPlus} />
-				</button>
-			</div>
-			<div className="w-full p-5 h-auto flex flex-wrap items-center justify-center">
-				{stickyNotes.length > 0 &&
-					stickyNotes.map((stickyNote: StickyNote, index: number) => {
-						return (
-							<StickyNoteComponent
-								stickyNote={stickyNote}
-								key={stickyNote._id || index}
-							/>
-						);
-					})}
+		<div className={`${theme === "dark" ? "dark" : ""}`}>
+			<div className="w-full p-3 dark:bg-slate-800 dark:text-slate-50 lg:h-[calc(100vh-3.5rem)]">
+				<div className="p-5 text-2xl">
+					<button
+						className="bg-slate-200 p-2 rounded-md border border-black w-12 h-12 flex items-center justify-center dark:bg-slate-500 dark:text-slate-50"
+						onClick={createStickyNote}
+					>
+						<FontAwesomeIcon icon={faPlus} />
+					</button>
+				</div>
+				<div className="w-full p-5 h-auto flex flex-wrap items-center justify-center">
+					{stickyNotes.length > 0 &&
+						stickyNotes.map((stickyNote: StickyNote, index: number) => {
+							return (
+								<StickyNoteComponent
+									stickyNote={stickyNote}
+									key={stickyNote._id || index}
+								/>
+							);
+						})}
+				</div>
 			</div>
 		</div>
 	);
