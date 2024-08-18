7ff9543d9c4f52ca7017bc7193e3c745525710fd
Hairum Qureshi <hqureshi@udel.edu>
2024-08-18 16:35:15 -0400

    removed pagination logic

--- frontend/src/components/NotesViewer.tsx
+++ frontend/src/components/NotesViewer.tsx
@@ -11,6 +11,7 @@ import useSessionContext from "../contexts/sessionContext";
 import { useEffect, useState } from "react";
 import { useTheme } from "../contexts/themeContext";
 import LoadingSpinner from "./LoadingSpinner";
+import Pagination from "./Pagination";
 
 export default function NotesViewer() {
 	const [numButtons, setNumButtons] = useState<number[]>([]);
@@ -156,49 +157,12 @@ export default function NotesViewer() {
 					</div>
 				)}
 			</div>
-			{totalNotes > 9 && (
-				<div className="w-full lg:absolute fixed bottom-0 p-5 flex items-center text-lg justify-center shadow-md">
-					<Link
-						to={
-							Number(pageNumber) <= 1
-								? "/notes/all"
-								: `?page=${Number(pageNumber) - 1}`
-						}
-						className={`w-9 h-9 rounded p-2 ${
-							Number(pageNumber) <= 1
-								? "pointer-events-none bg-red-800 dark:bg-red-800 text-white"
-								: "bg-black dark:bg-slate-900 text-white dark:text-slate-50"
-						} flex items-center justify-center mr-2`}
-					>
-						<FontAwesomeIcon icon={faArrowLeft} />
-					</Link>
-					{numButtons.map((buttonNo: number) => {
-						return (
-							<Link
-								key={buttonNo}
-								to={`?page=${buttonNo}`}
-								className={`w-9 h-9 rounded p-2 ${
-									Number(pageNumber) === buttonNo
-										? "bg-slate-400 dark:bg-slate-600 text-white"
-										: "bg-black dark:bg-slate-900 text-white dark:text-slate-50"
-								} flex items-center justify-center mr-2`}
-							>
-								{buttonNo}
-							</Link>
-						);
-					})}
-					<Link
-						to={`?page=${Number(pageNumber) + 1}`}
-						className={`w-9 h-9 rounded p-2 ${
-							Number(pageNumber) >= numPages
-								? "pointer-events-none bg-red-800 dark:bg-red-800 text-white"
-								: "bg-black dark:bg-slate-900 text-white dark:text-slate-50"
-						} flex items-center justify-center mr-2`}
-					>
-						<FontAwesomeIcon icon={faArrowRight} />
-					</Link>
-				</div>
-			)}
+			<Pagination
+				totalNotes={totalNotes}
+				pageNumber={pageNumber}
+				numPages={numPages}
+				numButtons={numButtons}
+			/>
 		</div>
 	);
 }
