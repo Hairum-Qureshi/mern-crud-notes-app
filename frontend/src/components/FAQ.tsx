f6ef6cd789d892f233aa32682f971e3a31583b59
Hairum Qureshi <hqureshi@udel.edu>
2024-08-17 22:06:09 -0400

    implemented logic for being able to view the answer to specific FAQs instead of them all

--- frontend/src/components/FAQ.tsx
+++ frontend/src/components/FAQ.tsx
@@ -9,34 +9,38 @@ interface FAQ_Interface {
 }
 
 export default function FAQ() {
-	const [showAnswer, setShowAnswer] = useState(false);
+	const [faqIndex, setFAQIndex] = useState(-1);
 
 	// TODO - move interface to 'interfaces.ts' file
 	// TODO - consider modifying/removing the report FAQ
 	// TODO - figure out why the link color isn't being added for the rules & disclaimers link
 	// TODO - make it so that you're able to click the link instead of the FAQ div closing
 	// TODO - make it so that you're able to open a specific FAQ instead of them all
+	// TODO - make the background fill the entire screen even on mobile view
+	// TODO - consider adding an 'FAQ' title or a 'Before Posting...' title
 
 	return faq_json.map((faq: FAQ_Interface, index: number) => {
 		return (
 			<div
 				className={`border-2 text-base bg-black text-white border-black dark:border-gray-500 rounded-md px-2 py-3 dark:bg-slate-800 hover:cursor-pointer ${
-					index !== 0 && "mt-3"
+					index !== 0 && "mt-4"
 				}`}
 				key={index}
-				onClick={() => setShowAnswer(!showAnswer)}
 			>
-				<div className="w-full flex">
+				<div
+					className="w-full flex"
+					onClick={() => setFAQIndex(faqIndex === index ? -1 : index)}
+				>
 					<h2 className="font-semibold text-lg">{faq.question}</h2>
 					<span className="ml-auto text-lg mr-2">
-						{showAnswer ? (
+						{faqIndex === index ? (
 							<FontAwesomeIcon icon={faAngleUp} />
 						) : (
 							<FontAwesomeIcon icon={faAngleDown} />
 						)}
 					</span>
 				</div>
-				{showAnswer && (
+				{faqIndex === index && (
 					<div
 						className="w-full dark:text-slate-400 text-gray-300 mt-2"
 						dangerouslySetInnerHTML={{ __html: faq.answer }}
