eaabf05fde8ff1abde8757a6ca82fe5caf010f12
Hairum Qureshi <hqureshi@udel.edu>
2024-08-19 09:54:54 -0400

    made rate limiter logic reusable so you can now pass in a custom number of max requests instead of the number being set in stone

--- backend/config/rate-limiter.ts
+++ backend/config/rate-limiter.ts
@@ -1,9 +1,11 @@
 import rateLimit from "express-rate-limit";
 
-const limit = rateLimit({
-	max: 10, // maximum requests
-	windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
-	message: "Too many requests. Please try again in 1 hour."
-});
+const rateLimiter = (maxRequests: number) => {
+	return rateLimit({
+		max: maxRequests,
+		windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
+		message: "Too many requests. Please try again in 1 hour."
+	});
+};
 
-export default limit;
+export default rateLimiter;
