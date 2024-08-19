75774f849bb3e6ef6a3e6c9bfce461c78b530818
Hairum Qureshi <hqureshi@udel.edu>
2024-08-19 11:50:51 -0400

    implemented logic for clearing inputs upon submissing and hiding the alert messages after a few seconds

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 export default function ContactForm() {
@@ -10,16 +10,25 @@ export default function ContactForm() {
 		message: ""
 	});
 
-	// TODO - figure out how to clear input after submitting
+	const subjectRef = useRef<HTMLInputElement>(null);
+	const emailRef = useRef<HTMLInputElement>(null);
+	const messageRef = useRef<HTMLTextAreaElement>(null);
+	const [showMessage, setShowMessage] = useState(false);
 
 	useEffect(() => {
-		setTimeout(() => {
-			setDisplayMessage({
-				messageType: "",
-				message: ""
-			});
-		}, 3000);
-	}, [displayMessage]);
+		if (showMessage) {
+			const timer = setTimeout(() => {
+				setShowMessage(false);
+				setDisplayMessage({
+					messageType: "",
+					message: ""
+				});
+			}, 2000);
+
+			// Cleanup the timeout if the component unmounts or if showMessage changes
+			return () => clearTimeout(timer);
+		}
+	}, [showMessage]);
 
 	async function sendEmail(e: FormEvent) {
 		e.preventDefault();
@@ -35,6 +44,12 @@ export default function ContactForm() {
 						messageType: "success",
 						message: response.data.message
 					});
+
+					subjectRef.current!.value = "";
+					emailRef.current ? (emailRef.current.value = "") : null;
+					messageRef.current!.value = "";
+
+					setShowMessage(true);
 				})
 				.catch(error => {
 					console.log(error);
@@ -42,12 +57,14 @@ export default function ContactForm() {
 						messageType: "error",
 						message: error.response.data.message || error.response.data
 					});
+					setShowMessage(true);
 				});
 		} else {
 			setDisplayMessage({
 				messageType: "error",
 				message: "Please be sure to provide a subject and message"
 			});
+			setShowMessage(true);
 		}
 	}
 
@@ -55,7 +72,7 @@ export default function ContactForm() {
 		<div>
 			<form className="h-full p-10">
 				<h1 className="lg:text-3xl text-4xl font-semibold">Contact Me!</h1>
-				{displayMessage.message && (
+				{showMessage && (
 					<div
 						className={`w-full p-2 my-2 rounded-md transition-opacity ease-in duration-100 ${
 							displayMessage.messageType === "error"
@@ -76,6 +93,7 @@ export default function ContactForm() {
 						placeholder="Subject"
 						className="w-full p-2 my-1 text-base border-2 border-gray-600 rounded dark:bg-gray-900 dark:border-gray-600 dark:text-gray-300 dark:focus:outline-none dark:focus:border-gray-500 dark:focus:border-2"
 						onChange={e => setSubject(e.target.value)}
+						ref={subjectRef}
 					/>
 				</div>
 				<div className="mt-3">
@@ -85,6 +103,7 @@ export default function ContactForm() {
 						placeholder="Email"
 						className="w-full p-2 my-1 text-base border-2 border-gray-600 rounded dark:bg-gray-900 dark:border-gray-600 dark:text-gray-300 dark:focus:outline-none dark:focus:border-gray-500 dark:focus:border-2"
 						onChange={e => setEmail(e.target.value)}
+						ref={emailRef}
 					/>
 				</div>
 				<div className="mt-3">
@@ -97,6 +116,7 @@ export default function ContactForm() {
 						placeholder="If you're reporting a bug, please be sure to list steps to reproduce it as well!"
 						className="w-full h-60 p-2 my-1 text-base border-2 border-gray-600 rounded dark:bg-gray-900 dark:border-gray-600 dark:text-gray-300 dark:focus:outline-none dark:focus:border-gray-500 dark:focus:border-2 resize-none"
 						onChange={e => setMessage(e.target.value)}
+						ref={messageRef}
 					></textarea>
 				</div>
 
