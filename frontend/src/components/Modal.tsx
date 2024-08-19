4b52b0d500343a6fcdd1eaeb7d96576226ad1f22
Hairum Qureshi <hqureshi@udel.edu>
2024-08-18 22:01:05 -0400

    updated props list and made delete button wrok

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,24 +1,39 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useNotes from "../hooks/useNotes";
 
 interface Props {
 	heading: string;
 	children: React.ReactNode;
 	modalType: string;
+	toggleModal: () => void;
+	noteID: string;
 }
 
 // TODO - make the buttons work
 // TODO - add hover effects on the buttons
 
-export default function Modal({ children, modalType, heading }: Props) {
+export default function Modal({
+	children,
+	modalType,
+	heading,
+	toggleModal,
+	noteID
+}: Props) {
 	const message = children as string;
+
+	const { deleteNote } = useNotes();
+
 	return (
 		<div
 			className={`m-auto lg:w-1/2 w-11/12 border-box text-white bg-slate-800 p-2 absolute left-0 right-0 top-32 rounded-md dark:bg-sky-950 dark:border dark:border-sky-400 ${
 				modalType !== "confirmation" && "pb-5"
 			}`}
 		>
-			<div className="w-full text-right text-xl hover:cursor-pointer">
+			<div
+				className="w-full text-right text-xl hover:cursor-pointer"
+				onClick={toggleModal}
+			>
 				<FontAwesomeIcon icon={faX} className="mr-2" />
 			</div>
 			<div className="m-2">
@@ -28,10 +43,19 @@ export default function Modal({ children, modalType, heading }: Props) {
 			{modalType === "confirmation" && (
 				<div className="w-full flex">
 					<div className="ml-auto p-3">
-						<button className="p-1 border-2 text-black bg-slate-200 border-slate-500 rounded-md w-28 mr-2">
+						<button
+							className="p-1 border-2 text-black bg-slate-200 border-slate-500 rounded-md w-28 mr-2"
+							onClick={toggleModal}
+						>
 							Cancel
 						</button>
-						<button className="p-1 border-2 bg-red-700 border-red-600 rounded-md w-28">
+						<button
+							className="p-1 border-2 bg-red-700 border-red-600 rounded-md w-28"
+							onClick={() => {
+								deleteNote(noteID);
+								toggleModal();
+							}}
+						>
 							Delete
 						</button>
 					</div>
