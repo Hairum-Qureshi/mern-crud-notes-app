e9e567e67c36d746945faee9136da7ed1bda8ce5
Hairum Qureshi <hqureshi@udel.edu>
2024-08-25 22:29:24 -0400

    removed hard-coded frontend URL for CORS and instead used an environment variable

--- backend/server.ts
+++ backend/server.ts
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-	origin: ["http://localhost:5173", "http://localhost:5174"],
+	origin: [process.env.FRONTEND_URL!],
 	credentials: true,
 	optionSuccessStatus: 200
 };
